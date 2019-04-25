dbed38e8d2b8109e60f016cc2e63e88b5df3d725
ErtugrulKucuk35 <ertgrl_35_@hotmail.com>
2019-04-25 22:54:08 +0300

    ikinci commitim

--- githubtest.c
+++ githubtest.c
@@ -1,13 +1,11 @@
 #include<stdio.h>
-int main(){
 
-
-
-printf("Merhaba git ve github");
-
-
-
-
-return 0 ;
+int main (){
+int a;
+printf("iki adet sayi giriniz...\n");
+scanf("%d",&a);
+if (a<=65 && a>=18)printf("girilen aralık 65 ve 18 aralıgında\n ");
+else printf("girilen value 65 ve 18 aralıgında degildir !!!!\n");
+  return 0 ;
 }
-//Ertugrul Kucuk...
+//Ertugrul kucuk ...
